38bd33bb0c4db76c1294fc1eed2f4d8e9fef3c08
J Silverstein <jess.pebble@gmail.com>
2017-04-04 15:27:28 -0400

    removing other semicolons

--- dom/app.js
+++ dom/app.js
@@ -59,38 +59,38 @@ function makeMiddleEarth() {
     place.appendChild(nameOfLand);
   }
   // append the section to the body of the DOM with: document.body.appendChild( // variable name )
-};
+}
 
 function makeHobbits() {
   console.log('Make hobbits');
   // display an unordered list of hobbits in the shire
   // give each hobbit a class of "hobbit"
   // list the hobbits alphabetically.  Maybe use the .sort() method.
-};
+}
 
 function keepItSecretKeepItSafe() {
   // create an empty div with an id of 'the-ring' and innerHTML of "The ring"
   // add the ring as a child of Frodo
-};
+}
 
 function makeBaddies() {
   // display an unordered list of baddies in Mordor
   // give each of the baddies a class of "baddy"
-};
+}
 
 function makeBuddies() {
   // create an aside tag and append it below mordor
   // display an unordered list of buddies in the aside
   // give each of the buddies a class of "buddy"
-};
+}
 
 function leaveTheShire() {
   // grab the hobbits and move them to Rivendell
-};
+}
 
 function beautifulStranger() {
   // change the buddy 'Strider' node to "Aragorn"
-};
+}
 
 function forgeTheFellowShip() {
   // move the hobbits and the buddies to Rivendell
@@ -98,26 +98,26 @@ function forgeTheFellowShip() {
   // add an h1 with the text 'The Fellowship' to this new div
   // add each hobbit and buddy one at a time to 'the-fellowship'
   // after each character is added make an alert that they have joined your party
-};
+}
 
 function theBalrog() {
   // change the inner HTML of the 'Gandalf' node to 'Gandalf the White'
   // add a class "the-white" to this element
   // in the style.css file, add a css rule for the class "the-white"
   // have a white background and a grey border
-};
+}
 
 function hornOfGondor() {
   // pop up an alert that the horn of gondor has been blown
   // Boromir's been killed by the Uruk-hai!
   // put a linethrough on boromir's name
   // Remove the Uruk-Hai from the Baddies on the page
-};
+}
 
 function itsDangerousToGoAlone(){
   // take Frodo and Sam out of the fellowship and move them to Mordor
   // add a div with an id of 'mount-doom' to Mordor
-};
+}
 
 function weWantsIt() {
   // Create a div with an id of 'gollum' and add it to Mordor
@@ -125,13 +125,13 @@ function weWantsIt() {
   // Create a condition (Maybe an IF statement) that changes the font-color of the ring div to Red if
   // it is a child of the div with the id 'gollum'
   // Move Gollum into Mount Doom
-};
+}
 
 function thereAndBackAgain() {
   // remove Gollum and the Ring from the DOM
   // remove all the baddies from the DOM
   // Move all the hobbits back to the shire
-};
+}
 
 // =================================
 // What to do when the page is ready
